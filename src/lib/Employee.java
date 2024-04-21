ae3a7d01d34b6a013bd25993e74d61cdf0ce50d0
tangguhlaks <tangguhlaksana0@gmail.com>
2024-04-21 14:56:19 +0700

    Menambahkan 2 function baru supaya function setMonthlySalary tidak bed smell long method

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -51,24 +51,26 @@ public class Employee {
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
-	
-	public void setMonthlySalary(int grade) {	
+
+	public void setBaseMonthlySalary(int grade) {
 		if (grade == 1) {
 			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
+		} else if (grade == 2) {
 			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
+		} else if (grade == 3) {
 			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
 		}
+	}
+
+	public void adjustForeignerSalary() {
+		if (isForeigner) {
+			monthlySalary *= 1.5;
+		}
+	}
+
+	public void setMonthlySalary(int grade) {
+		setBaseMonthlySalary(grade);
+		adjustForeignerSalary();
 	}
 	
 	public void setAnnualDeductible(int deductible) {	
